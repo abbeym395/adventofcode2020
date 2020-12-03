cf2186a1546e34a7aa66708196e43fb0bda10a0a
Abbey Scofield-Marlowe <abbeyb17@gmail.com>
2020-12-03 15:41:35 +0000

    day 2 part 1 solution

--- Day2/day2.py
+++ Day2/day2.py
@@ -3,3 +3,16 @@
 with open("input.txt", "r") as f:
     rawInput = f.readlines()
 
+validPasswords = 0
+
+for line in rawInput:
+    splitLine = line.split(" ")
+    minMax = splitLine[0].split("-")
+    letter = splitLine[1].rstrip(":")
+    password = splitLine[2].rstrip("\n")
+    
+    occurrences = password.count(letter)
+    if (occurrences >= int(minMax[0])) and (occurrences <= int(minMax[1])):
+        validPasswords += 1
+
+print("Valid passwords:", validPasswords)
