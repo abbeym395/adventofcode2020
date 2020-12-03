7720b35b29b4246e1784b97217216187484e4bcc
Abbey Scofield-Marlowe <abbeyb17@gmail.com>
2020-12-03 15:53:06 +0000

    Day 2 completed

--- Day2/day2.py
+++ Day2/day2.py
@@ -3,7 +3,7 @@
 with open("input.txt", "r") as f:
     rawInput = f.readlines()
 
-validPasswords = 0
+p1ValidPasswords = 0
 
 for line in rawInput:
     splitLine = line.split(" ")
@@ -13,6 +13,21 @@ for line in rawInput:
     
     occurrences = password.count(letter)
     if (occurrences >= int(minMax[0])) and (occurrences <= int(minMax[1])):
-        validPasswords += 1
+        p1ValidPasswords += 1
 
-print("Valid passwords:", validPasswords)
+print("Part 1 valid passwords:", p1ValidPasswords)
+
+p2ValidPasswords = 0
+
+for line in rawInput:
+    splitLine = line.split(" ")
+    positions = splitLine[0].split("-")
+    positions = list(map(int, positions))
+    
+    letter = splitLine[1].rstrip(":")
+    password = splitLine[2].rstrip("\n")
+    
+    if (password[positions[0]-1] == letter) ^ (password[positions[1]-1] == letter):
+        p2ValidPasswords += 1
+
+print ("Part 2 valid passwords:", p2ValidPasswords)
