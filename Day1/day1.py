650b06178289ae863492b64b6a67df98dcb99f12
Abbey Scofield-Marlowe <abbeyb17@gmail.com>
2020-12-03 12:16:54 +0000

    Day 1 Part 1 done

---
+++ Day1/day1.py
@@ -0,0 +1,20 @@
+## Advent of Code - Day 1, question 1
+
+with open("input.txt", "r") as f:
+    expensesStrings = f.readlines()
+
+expenses = []
+for line in expensesStrings:
+    expenses.append(int(line[:-1]))
+    
+expensesAsc = sorted(expenses)
+
+print("For part 1")
+for item in expensesAsc:
+    if 2020 - item in expensesAsc:
+        print("Numbers are", item, "and", 2020 - item)
+        print("Solution is", item * (2020 - item))
+        break
+    
+print("For part 2")
+print(expensesAsc)
